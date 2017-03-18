33e5a7d424685fc5d88b1c2e216c0b790b64310c
Awesomeness <athomield@hotmail.com>
2017-03-18 12:21:37 +0100

    v.9

---
+++ Debug/label.cpp.o.d
@@ -0,0 +1,10 @@
+Debug/label.cpp.o: label.cpp headers/label.h headers/sprite.h \
+ headers/window.h headers/textureLoader.h
+
+headers/label.h:
+
+headers/sprite.h:
+
+headers/window.h:
+
+headers/textureLoader.h:
